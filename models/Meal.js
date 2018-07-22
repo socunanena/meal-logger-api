a9f1d8d00c9c74c9d05cf2ab66028030b3b33cae
Natàlia <natalia.cavi@gmail.com>
2018-07-22 23:11:33 +0200

    Improved models definitions

--- models/Meal.js
+++ models/Meal.js
@@ -5,11 +5,16 @@ var Types = keystone.Field.Types;
  * Meal Model
  * ==========
  */
-var Meal = new keystone.List('Meal');
+var Meal = new keystone.List('Meal', {
+  defaultSort: '-date',
+  defaultColumns: 'date,content',
+  track: true,
+  map: { name: 'date' },
+});
 
 Meal.add({
   date: { type: Types.Datetime, required: true, initial: true },
-  content: { type: String, required: true, initial: true },
+  content: { type: Types.Textarea, required: true, initial: true },
 });
 
 /**
